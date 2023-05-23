b90ff14802feb393ad023670dfd47d6c2e745215
李鹏玺 <2899952565@qq.com>
2023-05-23 18:20:12 +0800

    test

--- vue/SimulatedResponsive/index.js
+++ vue/SimulatedResponsive/index.js
@@ -2,7 +2,9 @@
  * @Author       : 李鹏玺 2899952565@qq.com
  * @Date         : 2023-05-23 18:16:31
  * @LastEditors  : 李鹏玺 2899952565@qq.com
- * @LastEditTime : 2023-05-23 18:16:32
+ * @LastEditTime : 2023-05-23 18:20:01
  * @FilePath     : /vue/SimulatedResponsive/index.js
  * @Description  :
  */
+
+test;
