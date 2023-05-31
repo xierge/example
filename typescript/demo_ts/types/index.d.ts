36ed17dd038b5fa2830c7faf2e248fbe01c1783b
李鹏玺 <2899952565@qq.com>
2023-05-31 11:55:05 +0800

    🐞 fix: delete

--- typescript/demo_ts/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Date: 2023-05-30 17:56:07
- * @LastEditors: 李鹏玺 2899952565@qq.com
- * @LastEditTime: 2023-05-30 17:58:10
- * @FilePath: /example/typescript/demo_ts/type/index.d.ts
- * @description: 
- */
-
-export type getSum = (a:number,b:number)=>number
