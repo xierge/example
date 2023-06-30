8b1f801a3822520e098c29b169d69c508a91b55b
Carlos <2899952565@qq.com>
2023-06-30 17:51:47 +0800

    feat:

--- css/index.js
+++ css/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Date: 2023-06-29 16:20:37
+ * @LastEditors: Carlos 2899952565@qq.com
+ * @LastEditTime: 2023-06-29 18:14:03
+ * @FilePath: /example/css/index.js
+ * @description: 
+ */
 const childProcess = require("child_process");
 
 const fs = require("fs");
@@ -6,7 +13,7 @@ const path = require("path");
 
 function exec(command, callback) {
   childProcess.exec(
-    `npx sass ${common}/index.scss ${common}/index.css`,
+    command,
     function (err, stdout) {
       if (!err) {
         callback("编译成功");
@@ -18,10 +25,10 @@ function exec(command, callback) {
 fs.readdir(path.posix.resolve(__dirname, "src"), (err, demos) => {
   demos.forEach((demo) => {
     const common = path.resolve(__dirname, "src", demo);
-    exec(`npx sass ${common}/index.scss ${common}/index.css`);
+    exec(`npx sass ${common}/index.scss ${common}/index.css`,res=>console.log(res));
 
     fs.watchFile(path.resolve(common, "index.scss"), () => {
-      exec(`npx sass ${common}/index.scss ${common}/index.css`);
+      exec(`npx sass ${common}/index.scss ${common}/index.css`,res=>console.log(res));
     });
   });
 });
